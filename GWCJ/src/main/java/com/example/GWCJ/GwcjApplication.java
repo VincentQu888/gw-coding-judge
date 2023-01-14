d2a838be79e616ac9cc530c181d8845d6738c2ad
VincentQu888 <112832158+VincentQu888@users.noreply.github.com>
2023-01-14 17:56:19 -0500

    Add files via upload

---
+++ GWCJ/src/main/java/com/example/GWCJ/GwcjApplication.java
@@ -0,0 +1,13 @@
+package com.example.GWCJ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GwcjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GwcjApplication.class, args);
+	}
+
+}
---
+++ GWCJ/src/test/java/com/example/GWCJ/GwcjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GWCJ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GwcjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
